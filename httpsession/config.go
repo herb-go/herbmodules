b49899c9ce246086ecb7c9e08800db23718cd7e3
jarlyyn <jarlyyn@qq.com>
2020-11-24 21:51:09 +0800

    update

--- httpsession/config.go
+++ httpsession/config.go
@@ -1,7 +1,11 @@
 package httpsession
 
+var DefaultTimeout = int64(15 * 60)
+var DefaultMaxLifetime = int64(7 * 24 * 3600)
+var DefaultLastActiveInterval = int64(60)
+
 type Config struct {
-	AutoStart          bool
+	DisableAutoStart   bool
 	Engine             EngineName
 	EngineConfig       func(interface{}) error `config:", lazyload"`
 	MaxLifetime        int64
@@ -11,8 +15,20 @@ type Config struct {
 	InstallerConfig    func(interface{}) error `config:", lazyload"`
 }
 
+func setConfigDefaults(c *Config) {
+	if c.MaxLifetime == 0 {
+		c.MaxLifetime = DefaultMaxLifetime
+	}
+	if c.Timeout == 0 {
+		c.Timeout = DefaultTimeout
+	}
+	if c.LastActiveInterval == 0 {
+		c.LastActiveInterval = DefaultLastActiveInterval
+	}
+}
 func (c *Config) ApplyTo(s *Store) error {
-	s.AutoStart = c.AutoStart
+	setConfigDefaults(c)
+	s.AutoStart = !c.DisableAutoStart
 	s.MaxLifetime = c.MaxLifetime
 	s.Timeout = c.Timeout
 	s.LastActiveInterval = c.LastActiveInterval
